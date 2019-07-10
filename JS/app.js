e9fe2df0c4b2db3768bd32452394914774d459e9
smatthews-nz <sam@subsupps.co.nz>
2019-07-11 10:49:17 +1200

    Finalised fetchData method

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,32 @@
-//use the fetch API to request 12 random employes from english speaking countries for searching later
+//use the fetchData method to request 12 random employes from english speaking countries for searching later
 const employeesRequest = fetchData('https://randomuser.me/api/?results=12&nat=au,ca,gb,nz,us');
 
 //declare global variable to hold the employee objects
 let employees = [];
-//parse the 12 employee objects and store them in the variable
-employees = employeesRequest.map((employee, index) => {
-    employee.name = `${employeesRequest[index].name.first} ${employeesRequest[index.name.last]}`;
-    employees.push(employee);
-});
-
 console.log(employees);
 
+/**
+ * fetchData takes 
+ * @param (url) url or resource to be retrieved
+ */
 function fetchData(url){
     return fetch(url)
+    .then(checkStatus)
     .then(response => response.json())
+    .then(data => data.results.map(person => {
+        employees.push(person);
+    }))
     .catch(error => console.log("Looks like there was an error!", error));
 }
 
-function displayEmployee(data){
-
+function checkStatus(response){
+    if(response.ok){
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
 }
+
+// function displayEmployee(data){
+
+// }
