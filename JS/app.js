7c50ab06deae9a305e8f9892b2023b77719da6df
smatthews-nz <sam@subsupps.co.nz>
2019-07-11 16:26:35 +1200

    Appended employees to the DOM dynamically

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,21 @@
-//use the fetchData method to request 12 random employes from english speaking countries for searching later
-const employeesRequest = fetchData('https://randomuser.me/api/?results=12&nat=au,ca,gb,nz,us');
-
-//declare global variable to hold the employee objects
-let employees = [];
-console.log(employees);
+//get the gallery from the dom
+const gallery = $('#gallery');
+let modalDivs; 
 
 /**
- * fetchData takes 
+ * fetchData takes a url, creates a fetch request
+ * and returns the formatted data
+ * and pushes the data to an array of employees
  * @param (url) url or resource to be retrieved
  */
 function fetchData(url){
     return fetch(url)
     .then(checkStatus)
     .then(response => response.json())
-    .then(data => data.results.map(person => {
-        employees.push(person);
-    }))
+    .then(data => data.results.map(person => person))
     .catch(error => console.log("Looks like there was an error!", error));
 }
+fetchData('https://randomuser.me/api/?results=12&nat=au,ca,gb,nz,us').then(displayEmployees);
 
 function checkStatus(response){
     if(response.ok){
@@ -27,6 +25,29 @@ function checkStatus(response){
     }
 }
 
-// function displayEmployee(data){
+function displayEmployees(people){
+   people.map((person, index) =>{
 
-// }
+    console.log(person, index);
+    const card = $('<div>');
+    card.addClass('card');
+
+    card.html(`<div class="card-img-container">
+       <img class="card-img" src='${person.picture.large}' alt="profile picture">
+       </div>
+       
+       <div class="card-info-container">
+        <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+        <p class="card-text">${person.email}</p>
+        <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+       </div>
+       `);
+    
+    gallery.append(card);
+   });
+}
+
+function creatModal(people, index){
+
+
+}
